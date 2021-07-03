a67bae879e0e26f83d2f2d84e52b2ddca41bdc00
karimmfadel <karimmm.fadel@gmail.com>
2021-07-03 14:41:22 +0200

    validate the test cases fix the pom file issue fix the syntax of the SQL file

--- gateway-Service/src/test/java/com/tornado/gatewayService/GatewayServiceApplicationTests.java
+++ gateway-Service/src/test/java/com/tornado/gatewayService/GatewayServiceApplicationTests.java
@@ -1,9 +1,9 @@
 package com.tornado.gatewayService;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -28,6 +28,9 @@ import com.tornado.gatewayService.service.gateway.GatewayService;
 @SpringBootTest
 class GatewayServiceApplicationTests {
 
+	private static final Long GATEWAY_ID = 1L;
+	private static final Long DEVICE_ID = 1L;
+
 	@Autowired
 	private GatewayService gatewayService;
 
@@ -44,14 +47,16 @@ class GatewayServiceApplicationTests {
 	void contextLoads() {
 	}
 
-	
 	@Test
 	void addGatewayWithValidInput() {
+//		gatewayService.save(defineGateways().get(0));
+//		assertNotNull(gatewayService.findById(1L));
+
+		when(gatewayService.save(defineGateways().get(0))).thenReturn(defineGateways().get(0));
 		gatewayService.save(defineGateways().get(0));
-		assertNotNull(gatewayService.findById(1L));
+		verify(gatewayRepository, times(1)).save(defineGateways().get(0));
 	}
 
-	
 	@Test
 	void addGatewayWithInvalidInput() {
 
@@ -67,63 +72,39 @@ class GatewayServiceApplicationTests {
 		assertTrue(actualMessage.contains(expectedMessage));
 	}
 
-	
 	@Test
 	void returnListOfGateways() {
 		when(gatewayRepository.findAll()).thenReturn(defineGateways());
 		assertThat(gatewayService.findAll()).size().isGreaterThan(0);
 	}
 
-	
 	@Test
 	void returnListOfdevices() {
-		when(deviceRepository.findByGatewayId(1L)).thenReturn(defineDevices());
-		assertThat(deviceService.findByGatewayId(1L)).size().isGreaterThan(0);
+		when(deviceRepository.findByGatewayId(GATEWAY_ID)).thenReturn(defineDevices());
+		assertThat(deviceService.findByGatewayId(GATEWAY_ID)).size().isGreaterThan(0);
 	}
 
-	
 	@Test
 	void addDevice() {
-		Gateway gateway = defineDevices().get(0).getGateway();
-		gatewayService.save(gateway);
-		deviceService.save(defineDevices().get(0), gateway.getId());
-		assertNotNull(gatewayService.findById(1L));
+		deviceService.save(defineDevices().get(0), GATEWAY_ID);
+		verify(gatewayRepository, times(1)).getById(GATEWAY_ID);
 	}
-	
 
 	@Test
 	void removeDevice() {
-		when(deviceService.findById(1L)).thenReturn(defineDevices().get(0));
-		
-		deviceService.remove(1L);
-		
-		Exception exception = assertThrows(GatewayException.class, () -> {
-			deviceService.findById(1L);
-		});
+		when(deviceRepository.getById(DEVICE_ID)).thenReturn(defineDevices().get(0));
 
-		String expectedMessage = "invalid deviceId parameter";
-		String actualMessage = exception.getMessage();
+		deviceService.remove(DEVICE_ID);
 
-		assertTrue(actualMessage.contains(expectedMessage));
-		
-		verify(deviceRepository).deleteById(1L);
+		verify(deviceRepository, times(1)).getById(DEVICE_ID);
+		verify(deviceRepository, times(1)).deleteById(DEVICE_ID);
 	}
 
-	
 	private List<Gateway> defineGateways() {
 		List<Gateway> gateways = new ArrayList<>();
 
-		Gateway gateway = new Gateway();
-		gateway.setId(1L);
-		gateway.setIpAddress("10.0.1.0");
-		gateway.setName("Acer");
-		gateway.setUniqueSerialNumber("DFGF111111FD11");
-
-		Gateway gateway2 = new Gateway();
-		gateway2.setId(2L);
-		gateway2.setIpAddress("10.0.3.0");
-		gateway2.setName("Acer2");
-		gateway2.setUniqueSerialNumber("EEE2222FD11");
+		Gateway gateway = new Gateway(GATEWAY_ID, "DFGF111111FD11", "Acer", "10.0.1.0");
+		Gateway gateway2 = new Gateway(2L, "EEE2222FD11", "Acer2", "10.0.3.0");
 
 		gateways.add(gateway);
 		gateways.add(gateway2);
@@ -131,25 +112,13 @@ class GatewayServiceApplicationTests {
 		return gateways;
 	}
 
-	
 	private List<Device> defineDevices() {
 		List<Device> devices = new ArrayList<>();
 
-		Device device1 = new Device();
-		device1.setCreatedOn(new Date());
-		device1.setGateway(defineGateways().get(0));
-		device1.setId(1L);
-		device1.setStatus(DeviceStatus.OFFLINE);
-		device1.setUniqueNumber("124323453");
-		device1.setVendor("Ericsson");
-
-		Device device2 = new Device();
-		device2.setCreatedOn(new Date());
-		device2.setGateway(defineGateways().get(0));
-		device2.setId(2L);
-		device2.setStatus(DeviceStatus.OFFLINE);
-		device2.setUniqueNumber("124323453");
-		device2.setVendor("Ericsson");
+		Device device1 = new Device(DEVICE_ID, "124323453", "Ericsson", new Date(), DeviceStatus.OFFLINE,
+				defineGateways().get(0));
+		Device device2 = new Device(2L, "124323232", "Ericsson", new Date(), DeviceStatus.OFFLINE,
+				defineGateways().get(0));
 
 		devices.add(device1);
 		devices.add(device2);
